1563f08f8325559d1bbb41bc30bf093609ed4635
Sotiris2754 <t17dova@ionio.gr>
2023-04-25 17:27:56 +0300

    .

--- test.php
+++ test.php
@@ -23,6 +23,7 @@
 <script>
 	fetchContent(); // LOAD JSON FILE !!
 	retrieveData();
+	console.log();
 
 </script>
 
@@ -33,7 +34,8 @@
 
 		  </div> 
 	
-<a-scene id="scene">
+
+ <a-scene id="scene">
 
 				<a-assets>
 
@@ -45,27 +47,25 @@
 <a-sky color="lightblue"></a-sky>
 
 
+		<a-box id="1" show-list class="clickable" color="yellow" position="-16 0 -2.5" rotation="0 -90 0"></a-box> 
 
-		<a-box id="1" show-list class="clickable" color="yellow" position="-6 0 -5"></a-box> 
+		<a-box id="2" show-list class="clickable" color="red" position="-2 0 -7" rotation="0 -90 0"></a-box> <!--Έχω βάλει ελληνικά id και με κενό!!! -->
 
-		<a-box id="2" show-list class="clickable" color="red" position="-2 0 -5"></a-box> <!--Έχω βάλει ελληνικά id και με κενό!!! -->
-
-		<a-box id="3" show-list class="clickable" color="green" position="1.5 0 -5"></a-box>
+		<a-box id="3" show-list class="clickable" color="green" position="1 0 -7" rotation="0 90 0"></a-box>
 		 
-		<a-box id="4" show-list class="clickable" color="blue" position="5 0 -5"></a-box>
+		<a-box id="4" show-list class="clickable" color="blue" position="14.5 0 -2.5" rotation="0 90 0"></a-box>
 
 	<a-camera id="camera">
 		
-			<a-entity  raycaster="objects:.clickable" cursor="fuse:false; fuseTimeout:2000;" geometry="primitive:sphere; radius-inner:0.01; radius-outer:0.03; radius:0.03" material="color:orange;" position="0 0 -2.5;"  animation__color=" property:material.color; from:#FFA500 ; to: #00FF00; dur: 100; startEvents:mouseenter;" animation__coloreset=" property:material.color; from:#00FF00 ; to: #FFA500; dur: 100; startEvents:mouseleave;" animation__fusing=" property:scale; from: 1 1 1; to: .5 .5 .5; dur: 500; startEvents:mouseenter;" animation__reset="property:scale; to: 1 1 1; startEvents:mouseleave;">		
+			<a-entity  raycaster="objects:.clickable" cursor="fuse:false; fuseTimeout:2000;" geometry="primitive:sphere;radius:0.03" material="color:orange;" position="0 0 -2.5;"  animation__color=" property:material.color; from:#FFA500 ; to: #00FF00; dur: 100; startEvents:mouseenter;" animation__coloreset=" property:material.color; from:#00FF00 ; to: #FFA500; dur: 100; startEvents:mouseleave;" animation__fusing=" property:scale; from: 1 1 1; to: .5 .5 .5; dur: 500; startEvents:mouseenter;" animation__reset="property:scale; to: 1 1 1; startEvents:mouseleave;">		
 			</a-entity>
 		
 	</a-camera>
-<!-- <a-entity gltf-model="#building" scale="2 2 2" position="-15 -1 17" rotation="0 90 0"></a-entity> -->
+<a-entity gltf-model="#building" scale="2 2 2" position="-15 -0.5 17" rotation="0 90 0"></a-entity>
 
-</a-scene>
+</a-scene> 
 
 <script>
-
 
 </script>
 </body>
