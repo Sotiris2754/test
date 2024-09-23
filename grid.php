e6791d9f46652342c043b3888830cd63f60d3400
Sotiris2754 <t17dova@ionio.gr>
2024-09-23 23:21:42 +0300

    Update grid.php

--- grid.php
+++ grid.php
@@ -22,7 +22,7 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name'])){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 
 
-	<title>3V Grid</title>
+	<title>Exhibit Panel Changed</title>
 	<script src="OptBehavior.js"></script>
 
 
